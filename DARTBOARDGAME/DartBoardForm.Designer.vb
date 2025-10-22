0218feedc95e2785f5a280723c53b344593ada59
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-10-21 22:08:40 -0600

    Added Clear, Close, Log, and Clear history button. Added Data logging + Textbox with history Added ability to log data in a txt file yam

--- DARTBOARDGAME/DartBoardForm.Designer.vb
+++ DARTBOARDGAME/DartBoardForm.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class DartBoardForm
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,15 @@ Partial Class DartBoardForm
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.PictureBox = New System.Windows.Forms.PictureBox()
         Me.ThrowButton = New System.Windows.Forms.Button()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.LogButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ClearHistoryButton = New System.Windows.Forms.Button()
         CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -37,27 +42,82 @@ Partial Class DartBoardForm
         '
         'ThrowButton
         '
-        Me.ThrowButton.Location = New System.Drawing.Point(288, 297)
+        Me.ThrowButton.Location = New System.Drawing.Point(111, 297)
         Me.ThrowButton.Name = "ThrowButton"
         Me.ThrowButton.Size = New System.Drawing.Size(217, 151)
         Me.ThrowButton.TabIndex = 1
         Me.ThrowButton.Text = "THROW YAM!"
         Me.ThrowButton.UseVisualStyleBackColor = True
         '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(12, 297)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(93, 75)
+        Me.ClearButton.TabIndex = 2
+        Me.ClearButton.Text = "Clear YAMS!!!"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(491, 297)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(297, 141)
+        Me.TextBox1.TabIndex = 3
+        '
+        'LogButton
+        '
+        Me.LogButton.Location = New System.Drawing.Point(334, 297)
+        Me.LogButton.Name = "LogButton"
+        Me.LogButton.Size = New System.Drawing.Size(151, 75)
+        Me.LogButton.TabIndex = 4
+        Me.LogButton.Text = "YAM History"
+        Me.LogButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(12, 373)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(93, 75)
+        Me.ExitButton.TabIndex = 5
+        Me.ExitButton.Text = "Exit Yam Thrower"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'ClearHistoryButton
+        '
+        Me.ClearHistoryButton.Location = New System.Drawing.Point(334, 373)
+        Me.ClearHistoryButton.Name = "ClearHistoryButton"
+        Me.ClearHistoryButton.Size = New System.Drawing.Size(151, 75)
+        Me.ClearHistoryButton.TabIndex = 6
+        Me.ClearHistoryButton.Text = "CLEANSE YAM HISTORY"
+        Me.ClearHistoryButton.UseVisualStyleBackColor = True
+        '
         'DartBoardForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.ClearHistoryButton)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.LogButton)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.ThrowButton)
         Me.Controls.Add(Me.PictureBox)
         Me.Name = "DartBoardForm"
         Me.Text = "Form1"
         CType(Me.PictureBox, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents PictureBox As PictureBox
     Friend WithEvents ThrowButton As Button
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents LogButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents ClearHistoryButton As Button
 End Class
--- DARTBOARDGAME/DartBoardForm.vb
+++ DARTBOARDGAME/DartBoardForm.vb
@@ -1,4 +1,4 @@
-﻿'Option Strict On
+﻿Option Strict On
 Option Explicit On
 Option Compare Text
 'Alexis Villagran
@@ -15,6 +15,13 @@ Public Class DartBoardForm
     Public currentRound As Integer                 '""
     Public currentRoundToString As String          'Global Variables
 
+    '=================TRACKING & STORAGE=================
+    Private throwsThisTurn As New List(Of Point)
+    Private throwCount As Integer
+    Private logFile As String = IO.Path.Combine(
+    Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "DartGameLog.txt")
+
+
     '=================SET DEFAUTL VALUES=================
     Sub SetDefaultValues()
         PictureBox.Refresh()
@@ -26,7 +33,7 @@ Public Class DartBoardForm
     '=================FUNCTIONS=================
     Function RandomNumber(max%) As Integer
         Dim rndNumber As Integer
-        Randomize()
+        'Randomize()
         rndNumber = CInt(Int((max + 1) * Rnd()))
         Return rndNumber
     End Function
@@ -50,6 +57,34 @@ Public Class DartBoardForm
         Dim X As Integer = RandomNumber(PictureBox.Width)
         Dim Y As Integer = RandomNumber(PictureBox.Height)
         DartDraw(X, Y)
+
+
+        throwCount += 1
+        throwsThisTurn.Add(New Point(X, Y))
+
+        IO.File.AppendAllText(logFile, $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - Throw {throwCount}: ({X},{Y})" & Environment.NewLine)
+
+        currentRound = (throwCount - 1) Mod 3 + 1
+        currentRoundToString = CStr(currentRound)
+
+        If throwsThisTurn.Count = 3 Then
+            round += 1
+            roundString = CStr(round)
+
+            Dim sb As New System.Text.StringBuilder()
+            sb.AppendLine($"Round #{roundString}")
+
+            For i As Integer = 0 To throwsThisTurn.Count - 1
+                sb.AppendLine($"  Dart {i + 1} - ({throwsThisTurn(i).X}, {throwsThisTurn(i).Y})")
+            Next
+
+            IO.File.AppendAllText(logFile, Environment.NewLine)
+
+            sb.AppendLine()
+
+            If TextBox1 IsNot Nothing Then TextBox1.AppendText(sb.ToString())
+            throwsThisTurn.Clear()
+        End If
     End Sub
     '=================BUTTONS=================
     Private Sub ThrowButton_Click(sender As Object, e As EventArgs) Handles ThrowButton.Click
@@ -58,8 +93,89 @@ Public Class DartBoardForm
         ThrowDart()
     End Sub
 
+    Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        SetDefaultValues()
+
+        throwsThisTurn.Clear()
+        throwCount = 0
+        roundString = ""
+        currentRoundToString = ""
+
+        If TextBox1 IsNot Nothing Then TextBox1.Clear()
+
+        IO.File.AppendAllText(logFile, $"{Environment.NewLine}----- NEW GAME {DateTime.Now:yyyy-MM-dd HH:mm:ss} -----{Environment.NewLine}")
+    End Sub
+
+    Private Sub LogButton_Click(sender As Object, e As EventArgs) Handles LogButton.Click
+        Try
+            If IO.File.Exists(logFile) Then
+                Process.Start("notepad.exe", logFile)
+            Else
+                MessageBox.Show("No log file found yet.", "Missing Log", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error opening log file: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub ClearHistoryButton_Click(sender As Object, e As EventArgs) Handles ClearHistoryButton.Click
+
+        If MessageBox.Show(
+            "This will erase the entire YAM history file and reset counters. Do you really wish to cleanse the world of this?",
+            "Clear History",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Warning
+        ) = DialogResult.No Then
+            Return
+        End If
+
+        Try
+            ' Reset in-memory state
+            throwsThisTurn.Clear()
+            throwCount = 0
+            round = 0
+            currentRound = 0
+            roundString = ""
+            currentRoundToString = ""
+
+            ' Clear on-screen log
+            If TextBox1 IsNot Nothing Then TextBox1.Clear()
+
+            ' Truncate the log file (overwrite with a header)
+            IO.File.WriteAllText(logFile,
+                $"----- YAMS CLEANSED {DateTime.Now:yyyy-MM-dd HH:mm:ss} -----{Environment.NewLine}")
+
+            ' Optional: also reset the board visuals
+            'SetDefaultValues()
+
+            MessageBox.Show("YAM HISTORY CLEANSED.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show("ERROR CLEARING YAMS: " & ex.Message, "YAM :/",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+    '=================FORM LOAD=================
     Private Sub DartBoardForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         SetDefaultValues()
+        Randomize()
+
+        throwsThisTurn.Clear()
+        throwCount = 0
+        round = 0
+        currentRound = 0
+        roundString = ""
+        currentRoundToString = ""
+
+        If TextBox1 IsNot Nothing Then TextBox1.Clear()
+        If Not IO.File.Exists(logFile) Then IO.File.WriteAllText(logFile, "")
+        IO.File.AppendAllText(logFile, $"{Environment.NewLine}----- SESSION START {DateTime.Now:yyyy-MM-dd HH:mm:ss} -----{Environment.NewLine}")
+
     End Sub
+
 
 End Class
