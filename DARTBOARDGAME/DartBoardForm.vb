d66d982a550b6dd2447c5c35d50e2a567f106694
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-10-21 20:30:49 -0600

    Added Global Variables

--- DARTBOARDGAME/DartBoardForm.vb
+++ DARTBOARDGAME/DartBoardForm.vb
@@ -1,11 +1,21 @@
-﻿'Alexis Villagran
+﻿Option Strict On
+Option Explicit On
+Option Compare Text
+'Alexis Villagran
 '10/21/25
 'RCET
 
-Option Strict On
-Option Explicit On
-Option Compare Text
+Imports System.IO
+Imports System.Security.Cryptography.X509Certificates
 
 Public Class DartBoardForm
 
+    Public round As Integer                        'Global Variables
+    Public roundString As String                   '""
+    Public currentRound As Integer                 '""
+    Public currentRoundToString As String          'Global Variables
+
+    Private Sub DartBoardForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
